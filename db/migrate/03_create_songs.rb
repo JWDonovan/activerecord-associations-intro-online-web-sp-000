d74eb4f8814b6aba259cd8c42b854b3194394946
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-18 21:21:53 +0000

    Automatically backed up by Learn

--- db/migrate/03_create_songs.rb
+++ db/migrate/03_create_songs.rb
@@ -1,3 +1,9 @@
 class CreateSongs < ActiveRecord::Migration[4.2]
+  create_table :songs do |t|
+    t.string :name
+    t.integer :artist_id
+    t.integer :genre_id
+  end
+
   belongs_to :artist
 end
